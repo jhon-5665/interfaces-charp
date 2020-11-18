15e2dbd61024ef3725937b5ad750371ba8a1a484
Jhonatas Jardim <jhonatasjardim@hotmail.com>
2020-11-18 16:05:28 -0300

    Interface IOnlinePaymentService criada

--- Interfaces-csharp/Services/PaypalService.cs
+++ Interfaces-csharp/Services/PaypalService.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Interfaces_csharp.Services
+﻿namespace Interfaces_csharp.Services
 {
     class PaypalService
     {
+        public double Interest(double amount, int months)
+        {
+            return amount + 0.01 * months;
+        }
+        public double PaymentFee (double amount)
+        {
+            return amount + 0.02;
+        }                
     }
 }
